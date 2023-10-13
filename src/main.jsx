bfa898627b28cb435755da5dc5105fd3ad2a3859
phithanhdat <phithanhdathd@scuti.asia>
2023-10-13 18:13:11 +0700

    done, load, need add, edit and delete

---
+++ src/main.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import './index.css'
+import Root from './routes/root'
+import ErrorPage from './error-page'
+import Contact from './routes/contact'
+import { loader as rootLoader } from './routes/root'
+import { action as rootAction } from './routes/root'
+import {loader as contactLoader } from './routes/contact'
+
+const router = createBrowserRouter([
+  { 
+    path: '/', 
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    loader: rootLoader,
+    action: rootAction,
+    children: [
+      {
+        path: "contacts/:contactId",
+        element: <Contact />,
+        loader: contactLoader,
+      },
+    ]
+  },
+])
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
